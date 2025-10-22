267ff4bff20517b5a38931c2db793882db7c962a
lyw19b <liyawei@loongson.cn>
2025-10-22 09:06:21 +0800

    dev: rewrite aq_lsu_cfig.h

--- design/sys/core/lc164/lsu/rtl/aq_lsu_cfig.h
+++ design/sys/core/lc164/lsu/rtl/aq_lsu_cfig.h
@@ -22,50 +22,50 @@ limitations under the License.
 `define LSU_LD_BYTE_0   11
 
 `ifdef DPLEN_128
-  `define LSU_LD_BYTE_15    25+1
-  `define LSU_LD_SEXT       26+1
-  `define LSU_LD_ADDR_3     27+1
-  `define LSU_LD_WB_EN      28+1
-  `define LSU_LD_SIZE_0     29+1
-  `define LSU_LD_SIZE_1     30+1
-  `define LSU_LD_PTW        31+1
-  `define LSU_LD_LOCK       32+1
-  `define LSU_LD_SHIFT_0    33+1
-  `define LSU_LD_SHIFT_3    36+1
-  `define LSU_LD_STB_ID_0   37+1 
-  `define LSU_LD_STB_ID_1   38+1 
-  `define LSU_LD_AMO_INST   39+1 
-  `define LSU_LD_AMO_FUNC_0 40+1 
-  `define LSU_LD_AMO_FUNC_4 44+1 
+  `define LSU_LD_BYTE_15    26
+  `define LSU_LD_SEXT       27
+  `define LSU_LD_ADDR_3     28
+  `define LSU_LD_WB_EN      29
+  `define LSU_LD_SIZE_0     30
+  `define LSU_LD_SIZE_1     31
+  `define LSU_LD_PTW        32
+  `define LSU_LD_LOCK       33
+  `define LSU_LD_SHIFT_0    34
+  `define LSU_LD_SHIFT_3    37
+  `define LSU_LD_STB_ID_0   38 
+  `define LSU_LD_STB_ID_1   39 
+  `define LSU_LD_AMO_INST   40 
+  `define LSU_LD_AMO_FUNC_0 41 
+  `define LSU_LD_AMO_FUNC_4 45 
   `define LSU_LD_WIDTH      46 
   
-  `define LSU_DATAW       128 
-  `define LSU_BYTEW       16 
+  `define LSU_DATAW         128 
+  `define LSU_BYTEW         16 
 
-  `define LSU_VM_FFLEN    32 
-  `define VLSU_BLKW       32 
+  `define LSU_VM_FFLEN      32 
+  `define VLSU_BLKW         32 
 `else//DPLEN_64
     //LFB LDBUS
-  `define LSU_LD_BYTE_7     17+1
-  `define LSU_LD_SEXT       18+1
-  `define LSU_LD_ADDR_3     19+1
-  `define LSU_LD_WB_EN      20+1
-  `define LSU_LD_SIZE_0     21+1
-  `define LSU_LD_SIZE_1     22+1
-  `define LSU_LD_PTW        23+1
-  `define LSU_LD_LOCK       24+1
-  `define LSU_LD_SHIFT_0    25+1
-  `define LSU_LD_SHIFT_3    28+1
-  `define LSU_LD_STB_ID_0   29+1
-  `define LSU_LD_STB_ID_1   30+1 
-  `define LSU_LD_AMO_INST   31+1 
-  `define LSU_LD_AMO_FUNC_0 32+1 
-  `define LSU_LD_AMO_FUNC_4 36+1
+  `define LSU_LD_BYTE_7     18
+  `define LSU_LD_SEXT       19
+  `define LSU_LD_ADDR_3     20
+  `define LSU_LD_WB_EN      21
+  `define LSU_LD_SIZE_0     22
+  `define LSU_LD_SIZE_1     23
+  `define LSU_LD_PTW        24
+  `define LSU_LD_LOCK       25
+  `define LSU_LD_SHIFT_0    26
+  `define LSU_LD_SHIFT_3    29
+  `define LSU_LD_STB_ID_0   30
+  `define LSU_LD_STB_ID_1   31 
+  `define LSU_LD_AMO_INST   32 
+  `define LSU_LD_AMO_FUNC_0 33 
+  `define LSU_LD_AMO_FUNC_4 37
   `define LSU_LD_WIDTH      38
   
-  `define LSU_DATAW       64 
-  `define LSU_BYTEW       8 
+  `define LSU_DATAW         64 
+  `define LSU_BYTEW         8 
   
-  `define LSU_VM_FFLEN    16 
-  `define VLSU_BLKW       16 
+  `define LSU_VM_FFLEN      16 
+  `define VLSU_BLKW         16 
 `endif
